0b151c555e6370e270c0c3897356639c9017bcb9
Akash Raveendran <akashraveendran1611@gmail.com>
2023-05-19 13:31:11 +0530

    Completed backend

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+const connectDB = require("./db/connection")
+
+//Routes
+const taskRouter = require("./routes/task")
+
+
+//middlewares
+app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/v1/tasks', taskRouter)
+
+app.get("/", (req, res) => {
+    res.redirect("/index.html")
+})
+app.get("/test", (req, res) => {
+    res.send('Hello')
+})
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, (err) => {
+            if (err) console.log(err);
+            else console.log("Server Running at port 3000...");
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
